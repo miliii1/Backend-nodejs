99b352f9484d875c2919a72cd513c68f813c3615
Milagros Jara <milagrosjarag20@gmail.com>
2025-05-20 17:40:48 +0000

    ejercicio01.js

---
+++ javascript/Ejercicios/basico/ejercicio01.js
@@ -0,0 +1,28 @@
+let name = "Julia";
+let age = 20;
+let isTrue = true;
+let obj = {
+    name: "Aidan",
+    age: 21,
+    isTrue: false,
+    country: "Arg"
+}
+
+console.log(name);
+console.log(obj);
+console.log(age);
+
+/*
+const COLOR_RED =rgb(230, 25, 55);
+const COLOR_YELLOW =rgb(230, 25, 55);
+const COLOR_GREEN =rgb(230, 25, 55);
+*/
+let num1 = 5;
+let num2 = 3;
+let num3 = 4;
+let num4 = 7;
+console.log(num1 + num2);
+console.log(num1 - num3);
+console.log(num3 * num2);
+console.log(num1 / num2);
+console.log(num4 % num2);
